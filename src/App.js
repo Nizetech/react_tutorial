b1e0f6dc5197f92fe756065d3b7002092d4856d6
Fortunize <96587252+Fortunize@users.noreply.github.com>
2023-04-28 22:29:54 +0100

    usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    if (count < 10) setCount(count + 1);
+    console.log(count);
+  }
+
+  function handleSub() {
+    if (count > 0) setCount(count - 1);
+    console.log(count);
+  }
+
+  function handleReset() {
+    setCount(0);
+    console.log(count);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p> {count} </p>{" "}
+        <button className="add" onClick={handleAdd}>
+          ADD{" "}
+        </button>{" "}
+        <button className="sub" onClick={handleSub}>
+          SUB{" "}
+        </button>{" "}
+        <button className="reset" onClick={handleReset}>
+          SUB{" "}
+        </button>{" "}
+      </div>{" "}
     </div>
   );
 }
